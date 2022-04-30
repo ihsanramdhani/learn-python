09c75c76015a97008c75930107096e6a72ef7267
ihsanramdhani <ihsann.ramdhani@gmail.com>
2022-04-30 14:46:23 +0700

    Algorithm

---
+++ a_plus_abs_b.py
@@ -0,0 +1,23 @@
+from operator import sub, add
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> a_plus_abs_b(-1, 4)
+    3
+    >>> a_plus_abs_b(-1, -4)
+    3
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()

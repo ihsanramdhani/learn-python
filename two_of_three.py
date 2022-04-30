15e86284e83840c2441d069461c76a354c4f8321
ihsanramdhani <ihsann.ramdhani@gmail.com>
2022-05-01 00:38:24 +0700

    Algorithm

---
+++ two_of_three.py
@@ -0,0 +1,19 @@
+def two_of_three(i, j, k):
+    """Return m*m + n*n, where m and n are the two smallest members of the
+    positive numbers i, j, and k.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return i**2 + j**2 + k**2 - max(i, j, k)**2
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
